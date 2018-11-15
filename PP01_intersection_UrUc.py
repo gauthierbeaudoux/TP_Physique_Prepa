e160ffb9d813bafdb7d3f810b6e66299643631ac
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2018-11-15 09:52:00 +0100

    Initial commit

---
+++ PP01_intersection_UrUc.py
@@ -0,0 +1,16 @@
+# Pour la lecture
+import numpy as np
+# Lecture effective du fichier
+mesures_t,mesures_uc,mesures_ur = np.loadtxt('mesures_UrUc.txt',unpack=True)
+
+# A vous de jouer en definissant la fonction intersection 
+
+
+
+# et en l'appliquant  sur les listes precedentes.
+
+t_inter = 'Pas ca...' # A modifier
+
+print(t_inter)
+
+
